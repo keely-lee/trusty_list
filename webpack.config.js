3f7312eb5f0e356de88afb7bf380c32e318a5f0b
keely_lee <keely_lee@outlook.com>
2021-03-26 14:21:17 -0400

    Edit dependencies, entry points, config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/trusty_list.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', "assets", "javascripts"),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    inline: false,
+    contentBase: "./dist",
+  },
+}
